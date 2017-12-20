f8114e5f1f5f539b66180c81922351cb869f7fa9
afl1 <afl2001@gmail.com>
2017-12-20 13:05:17 +0100

    kodi: bump to g7b1ff23 to fix missing FILE_PROPERTY_EFFECTIVE_URL

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="9bc8607"
-PKG_SHA256="06a3f6552e40b821b92f8de057fb03dd0e96e1f10b24e213f03f75faa2cab133"
+PKG_VERSION="7b1ff23"
+PKG_SHA256="12106cdf7348754e836098188b9ea278b631695d28a0c85db40dd7e72950bc98"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
