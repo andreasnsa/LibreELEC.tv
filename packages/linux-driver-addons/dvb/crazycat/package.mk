b2ea12b6b22e4c9c2f4a846f09991603e2d09f86
afl1 <afl2001@gmail.com>
2018-01-06 15:13:32 +0000

    packages/projects: readd support for dvb on S905

--- packages/linux-driver-addons/dvb/crazycat/package.mk
+++ packages/linux-driver-addons/dvb/crazycat/package.mk
@@ -19,6 +19,7 @@
 PKG_NAME="crazycat"
 PKG_VERSION="2017-12-06"
 PKG_SHA256="779c7d42e5fd4dfac8f53654ce8af467d22a81b6c0b21e24f14aaaed033c6eb1"
+PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/crazycat69/linux_media"
@@ -30,10 +31,16 @@ PKG_SECTION="driver.dvb"
 PKG_LONGDESC="DVB driver for TBS cards with CrazyCats additions."
 
 PKG_IS_ADDON="yes"
+PKG_IS_KERNEL_PKG="yes"
 PKG_ADDON_IS_STANDALONE="yes"
 PKG_ADDON_NAME="DVB drivers for TBS (CrazyCat)"
 PKG_ADDON_TYPE="xbmc.service"
 PKG_ADDON_VERSION="${ADDON_VERSION}.${PKG_REV}"
+
+if [ "$PROJECT" = "Amlogic" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dvb_tv-aml"
+fi
+listcontains "$ADDITIONAL_DRIVERS" "wetekdvb" && PKG_DEPENDS_TARGET+=" wetekdvb" || true
 
 pre_make_target() {
   export KERNEL_VER=$(get_module_dir)
@@ -48,9 +55,41 @@ make_target() {
     if [ -f $PKG_DIR/config/generic.config ]; then
       cp $PKG_DIR/config/generic.config v4l/.config
     fi
-  else
+  elif [ "$PROJECT" != "Amlogic" ]; then
     if [ -f $PKG_DIR/config/usb.config ]; then
       cp $PKG_DIR/config/usb.config v4l/.config
+    fi
+  else
+
+    # Amlogic AMLVIDEO driver
+    if [ -e "$(kernel_path)/drivers/amlogic/video_dev" ]; then
+
+      # Copy, patch and enable amlvideodri module
+      cp -a "$(kernel_path)/drivers/amlogic/video_dev" "linux/drivers/media/"
+      sed -i 's,common/,,g; s,"trace/,",g' $(find linux/drivers/media/video_dev/ -type f)
+      sed -i 's,\$(CONFIG_V4L_AMLOGIC_VIDEO),m,g' "linux/drivers/media/video_dev/Makefile"
+      echo "obj-y += video_dev/" >> "linux/drivers/media/Makefile"
+
+      # Copy and enable videobuf-res module
+      cp -a "$(kernel_path)/drivers/media/v4l2-core/videobuf-res.c" "linux/drivers/media/v4l2-core/"
+      cp -a "$(kernel_path)/include/media/videobuf-res.h" "linux/include/media/"
+      echo "obj-m += videobuf-res.o" >> "linux/drivers/media/v4l2-core/Makefile"
+    fi
+
+    # Amlogic DVB drivers
+    if [ "$PROJECT" = "Amlogic" ]; then
+      DVB_TV_AML_DIR="$(get_build_dir dvb_tv-aml)"
+      if [ -d "$DVB_TV_AML_DIR" ]; then
+        cp -a "$DVB_TV_AML_DIR" "linux/drivers/media/dvb_tv"
+        echo "obj-y += dvb_tv/" >> "linux/drivers/media/Makefile"
+      fi
+      if listcontains "$ADDITIONAL_DRIVERS" "wetekdvb"; then
+        WETEKDVB_DIR="$(get_build_dir wetekdvb)"
+        if [ -d "$WETEKDVB_DIR" ]; then
+          echo "obj-y += amlogic/dvb_tv/" >> "linux/drivers/media/Makefile"
+          cp -a "$WETEKDVB_DIR/wetekdvb.ko" "v4l/"
+        fi
+      fi
     fi
   fi
 
