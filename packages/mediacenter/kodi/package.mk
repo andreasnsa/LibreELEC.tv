4b4e18fbba1d38cce26e86da7cf98af505d95983
Adam Green <greena88@gmail.com>
2018-01-28 03:10:43 +0000

    kodi: sync version from old branch

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="dc587bd"
-PKG_SHA256="91c41ecf4ef676cdc3259c6bd03d413518f980f5fdae0ab8d580865ab33e0944"
+PKG_VERSION="a3f4c40"
+PKG_SHA256="d7d997c44652735e80ae4106537619200e23b464875a1ceaacbc4ad53633f5f3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
